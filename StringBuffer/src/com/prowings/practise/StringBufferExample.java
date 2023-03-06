80afb57123e376f42c8f84f6367ac510c28e43e2
karanbhore <karanbhore64@gmail.com>
2023-03-06 23:49:35 +0530

    my change

---
+++ StringBuffer/src/com/prowings/practise/StringBufferExample.java
@@ -0,0 +1,18 @@
+package com.prowings.practise;
+public class StringBufferExample {
+    public static void main(String[] args) {
+    StringBuilder sb=new StringBuilder();
+    
+    sb.append("Hello");
+    sb.append(", World");
+    String msg=sb.toString();
+    System.out.println(msg);
+    
+    
+    
+    
+    
+    
+    
+    }
+}
